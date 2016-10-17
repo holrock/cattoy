acec79e572610dbe71d7540d78f991c35d619bc4
a <b>
2016-10-17 20:49:19 +0900

    add cap

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'cattoy'
+set :repo_url, 'https://github.com/holrock/cattoy.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/dep/cattoy'
+set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+set :rails_env, 'staging'
+set :migration_role, :db
+set :migration_servers, -> { primary(fetch(:migration_role)) }
+set :conditionally_migrate, true
+set :assets_roles, [:app]
+#set :assets_prefix, 'assets'
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,6 +1,6 @@
 application_path = File.expand_path('../../..', __FILE__).to_s
 directory application_path
-bind "unix://var/tmp/sockets/puma.sock"
+bind "unix:///var/tmp/puma.sock"
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 environment ENV.fetch('RAILS_ENV') { 'production' }
