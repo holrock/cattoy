38285fea3cab563e3157952a7b510a2eed22be65
holrock <ispeporez@gmail.com>
2016-10-31 04:27:16 +0900

    add omac server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  IMAGE_HOST = Cattoy::Application.config.image_host
+
+  def image_proxy_url(url)
+    digest = OpenSSL::HMAC.hexdigest('sha1', Rails.application.secrets.omac_key, url)
+    "#{IMAGE_HOST}/#{digest}?url=#{Rack::Utils.escape(url)}"
+  end
 end
