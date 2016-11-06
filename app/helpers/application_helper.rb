c3e6785e7de213a9731f10b973153d0e49c78ead
holrock <ispeporez@gmail.com>
2016-11-06 21:26:14 +0900

    xss!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,27 @@
 module ApplicationHelper
   IMAGE_HOST = Cattoy::Application.config.image_host
 
-  def image_proxy_url(url)
+  def self.build_image_proxy_url(url)
     digest = OpenSSL::HMAC.hexdigest('sha1', Rails.application.secrets.omac_key, url)
     "#{IMAGE_HOST}/#{digest}?url=#{Rack::Utils.escape(url)}"
+  end
+
+  def image_proxy_url(url)
+    ApplicationHelper.build_image_proxy_url(url)
+  end
+
+  SrcScrubber = Loofah::Scrubber.new do |node|
+    if node.name == 'img'
+      node['src'] = build_image_proxy_url(node['src'])
+    end
+  end
+
+  TAGS = %w(a img).freeze
+  ATTRS = %w(href src width height).freeze
+
+  def comment_format(history)
+    sanitized = sanitize(history.comment, tags: TAGS, attributes: ATTRS)
+    sanitized = sanitize(sanitized, scrubber: SrcScrubber)
+    simple_format(sanitized)
   end
 end
