9b63ecabc6eb8127e8ec5239acebb5e47d6d3040
holrock <ispeporez@gmail.com>
2018-08-13 00:37:09 +0900

    webpack

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+<template>
+   <v-app>
+     <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-title>Cattoy</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
